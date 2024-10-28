25b99afe3ffc7e60324d805c2908b71d87a9c51c
John Chen <john_chen@calcomp.com.tw>
2024-10-28 15:02:56 +0800

    Add support visa.

--- main_window_visa.py
+++ main_window_visa.py
@@ -1,29 +1,27 @@
 import sys
 import os
-import serial
-import serial.tools.list_ports
 from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout
 from PySide6.QtGui import QIcon, QCloseEvent
 from PySide6.QtCore import QThread, Signal
-from main_window_ui import Ui_MainWindow
+from main_window_visa_ui import Ui_MainWindow
 from password_dialog import PasswordDialog
 import resources_rc
+import pyvisa
 
 
 class SerialThread(QThread):
     data_received = Signal(str)
 
-    def __init__(self, serial_port: serial.Serial):
+    def __init__(self, serial_port: pyvisa.Resource):
         super().__init__()
         self.serial_port = serial_port
         self.running = True
 
     def run(self):
         while self.running:
-            if self.serial_port and self.serial_port.is_open and self.serial_port.in_waiting > 0:
-                received = self.serial_port.readline()
-                data = received.decode("utf-8").strip()
+            if self.serial_port:
+                received = self.serial_port.read()
+                data = received.strip()
                 self.data_received.emit(data)
 
     def stop(self):
@@ -67,27 +65,43 @@ class SerialTool(QMainWindow):
 
         self.serial_port = None
         self.serial_thread = None
+        self.resource_manager = None
 
     def populate_com_ports(self):
-        ports = serial.tools.list_ports.comports()
+        self.ui.com_combo.clear()
+        # 建立 Resource Manager
+        self.resource_manager = pyvisa.ResourceManager("@py")
+        # 列出所有可用資源
+        ports = self.resource_manager.list_resources()
         for port in ports:
-            if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
-                if "USB" in port.description.upper() or "USB" in port.hwid.upper():
-                    self.ui.com_combo.addItem(port.device)
-                    if not os.access(port.device, os.R_OK) or not os.access(port.device, os.W_OK):
-                        dialog = PasswordDialog(device=port.device)
-                        dialog.exec()
-            else:
-                self.ui.com_combo.addItem(port.device)
+            if "USB" in port or "ASRL" in port:
+                self.ui.com_combo.addItem(port)
+                # 無法取得路徑來設定存取權限
+                # if not os.access(port.device, os.R_OK) or not os.access(port.device, os.W_OK):
+                #     dialog = PasswordDialog(device=port.device)
+                #     dialog.exec()
 
+    def initialize_visa(self, com_port: str, baudrate: int, data_bits: int): 
+        try:
+            self.resource_manager = pyvisa.ResourceManager("@py")
+            self.serial_port = self.resource_manager.open_resource(com_port) 
+            self.serial_port.baud_rate = baudrate  # 設置與 Arduino 一致的波特率
+            self.serial_port.data_bits = data_bits
+            self.serial_port.parity = pyvisa.constants.Parity.none
+            self.serial_port.stop_bits = pyvisa.constants.StopBits.one
+            self.serial_port.timeout = 5000  # 設置逾時為 5 秒
+            return True
+        except Exception as e:
+            print("Error:", e)
+        return False
+    
     def connect_serial(self):
         """Connect to selection COM Port"""
         com_port = self.ui.com_combo.currentText()
         baudrate = self.ui.baudrate_spinbox.value()
         data_bits = int(self.ui.data_bits_combo.currentText())
         try:
-            if com_port != "":
-                self.serial_port = serial.Serial(com_port, baudrate, data_bits, timeout=1)
+            if com_port != "" and self.initialize_visa(com_port, baudrate, data_bits):
                 self.ui.send_button.setEnabled(True)
                 self.ui.connect_button.setEnabled(False)
                 self.ui.disconnect_button.setEnabled(True)
@@ -104,7 +118,7 @@ class SerialTool(QMainWindow):
     def disconnect_serial(self):
         """Disconnect COM Port"""
         try:
-            if self.serial_port and self.serial_port.is_open:
+            if self.serial_port:
                 self.ui.send_button.setEnabled(False)
                 self.ui.connect_button.setEnabled(True)
                 self.ui.disconnect_button.setEnabled(False)
@@ -120,16 +134,13 @@ class SerialTool(QMainWindow):
 
     def refresh_ports(self):
         """Reflash available COM Port list"""
-        self.ui.com_combo.clear()
-        ports = serial.tools.list_ports.comports()
-        for port in ports:
-            self.ui.com_combo.addItem(port.device)
+        self.populate_com_ports()
 
     def send_data(self):
         """Send serial command"""
         data = self.ui.command_edit.text()
-        if self.serial_port and self.serial_port.is_open and data != "":
-            self.serial_port.write(data.encode())
+        if self.serial_port and data != "":
+            self.serial_port.write(data)
             self.ui.receiver_text.append("Send: " + data)
 
     def receive_data(self, data: str):
---
+++ visa_test.py
@@ -0,0 +1,32 @@
+import pyvisa
+import time
+
+if __name__ == "__main__":
+    arduino = None
+    # 建立 Resource Manager
+    rm = pyvisa.ResourceManager("@py")
+
+    # 列出所有可用資源
+    print("Available resources:", rm.list_resources())
+
+    # 連接至 Arduino 的 COM 埠
+    try:
+        
+        arduino = rm.open_resource(rm.list_resources()[0]) 
+        arduino.baud_rate = 9600  # 設置與 Arduino 一致的波特率
+        arduino.data_bits = 8
+        arduino.parity = pyvisa.constants.Parity.none
+        arduino.stop_bits = pyvisa.constants.StopBits.one
+        arduino.timeout = 5000  # 設置逾時為 5 秒
+        time.sleep(2)
+        
+        # 發送資料並接收回應
+        arduino.write("-a\n")
+        time.sleep(1)
+        response = arduino.read()
+        print("Arduino responded:", response)
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        if arduino:
+            arduino.close()
